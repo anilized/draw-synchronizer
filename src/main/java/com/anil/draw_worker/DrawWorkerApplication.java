9327961f84c46209f3d343f5b77c30a9c2bd8916
Anıl Can <anillcan7@gmail.com>
2024-07-19 01:10:59 +0300

    init

---
+++ src/main/java/com/anil/draw_worker/DrawWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.anil.draw_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anil/draw_worker/DrawWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.draw_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
